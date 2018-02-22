8f5c1edb863fc80533c38baa928b3606bbe9a68d
HaddyYang <624644925@qq.com>
2018-02-23 00:15:25 +0800

    创建git项目和上传第17节的代码

---
+++ 17.博客后台富文本编辑/blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+from ckeditor_uploader.fields import RichTextUploadingField
+
+class BlogType(models.Model):
+    type_name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.type_name
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)
+    content = RichTextUploadingField()
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    readed_num = models.IntegerField(default=0)
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "<Blog: %s>" % self.title
+
+    class Meta:
+        ordering = ['-created_time']
