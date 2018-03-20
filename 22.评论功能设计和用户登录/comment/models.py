542aea1c35a9d716ba6f13057b9b5d0d3d70c4ad
HaddyYang <624644925@qq.com>
2018-03-21 03:13:07 +0800

    上传第22节课的代码

---
+++ 22.评论功能设计和用户登录/comment/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Comment(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.DO_NOTHING)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    text = models.TextField()
+    comment_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    
