07804c08e8f231567376e5ba3ef92b912af90931
HaddyYang <624644925@qq.com>
2018-04-18 02:00:55 +0800

    上传第27节课的代码

---
+++ 27.获取评论数和细节处理/comment/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Comment(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    text = models.TextField()
+    comment_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, related_name="comments", on_delete=models.CASCADE)
+
+    root = models.ForeignKey('self', related_name='root_comment', null=True, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', related_name='parent_comment', null=True, on_delete=models.CASCADE)
+    reply_to = models.ForeignKey(User, related_name="replies", null=True, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ['comment_time']
---
+++ 27.获取评论数和细节处理/mysite/views.py
@@ -0,0 +1,77 @@
+import datetime
+from django.shortcuts import render, redirect
+from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
+from django.db.models import Sum
+from django.core.cache import cache
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from read_statistics.utils import get_seven_days_read_data, get_today_hot_data, get_yesterday_hot_data
+from blog.models import Blog
+from .forms import LoginForm, RegForm
+
+def get_7_days_hot_blogs():
+    today = timezone.now().date()
+    date = today - datetime.timedelta(days=7)
+    blogs = Blog.objects \
+                .filter(read_details__date__lt=today, read_details__date__gte=date) \
+                .values('id', 'title') \
+                .annotate(read_num_sum=Sum('read_details__read_num')) \
+                .order_by('-read_num_sum')
+    return blogs[:7]
+
+def home(request):
+    blog_content_type = ContentType.objects.get_for_model(Blog)
+    dates, read_nums = get_seven_days_read_data(blog_content_type)
+
+    # 获取7天热门博客的缓存数据
+    hot_blogs_for_7_days = cache.get('hot_blogs_for_7_days')
+    if hot_blogs_for_7_days is None:
+        hot_blogs_for_7_days = get_7_days_hot_blogs()
+        cache.set('hot_blogs_for_7_days', hot_blogs_for_7_days, 3600)
+
+    context = {}
+    context['dates'] = dates
+    context['read_nums'] = read_nums
+    context['today_hot_data'] = get_today_hot_data(blog_content_type)
+    context['yesterday_hot_data'] = get_yesterday_hot_data(blog_content_type)
+    context['hot_blogs_for_7_days'] = hot_blogs_for_7_days
+    return render(request, 'home.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            user = login_form.cleaned_data['user']
+            auth.login(request, user)
+            return redirect(request.GET.get('from', reverse('home')))
+    else:
+        login_form = LoginForm()
+
+    context = {}
+    context['login_form'] = login_form
+    return render(request, 'login.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        reg_form = RegForm(request.POST)
+        if reg_form.is_valid():
+            username = reg_form.cleaned_data['username']
+            email = reg_form.cleaned_data['email']
+            password = reg_form.cleaned_data['password']
+            # 创建用户
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            # 登录用户
+            user = auth.authenticate(username=username, password=password)
+            auth.login(request, user)
+            return redirect(request.GET.get('from', reverse('home')))
+    else:
+        reg_form = RegForm()
+
+    context = {}
+    context['reg_form'] = reg_form
+    return render(request, 'register.html', context)
+    
