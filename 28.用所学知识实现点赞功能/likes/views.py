3cf5b2c3918ab7ab187dd8b3d4eff6b7f242aa60
HaddyYang <624644925@qq.com>
2018-04-26 11:50:35 +0800

    上传第28节课的代码

--- 28.用所学知识实现点赞功能/likes/views.py
+++ 28.用所学知识实现点赞功能/likes/views.py
@@ -19,43 +19,48 @@ def SuccessResponse(liked_num):
     return JsonResponse(data)
 
 def like_change(request):
+    # 获取数据
     user = request.user
     if not user.is_authenticated:
-        return ErrorResponse(400, 'user is not login')
+        return ErrorResponse(400, 'you were not login')
+
+    content_type = request.GET.get('content_type')
+    object_id = int(request.GET.get('object_id'))
 
     try:
-        content_type = request.GET.get('content_type')
         content_type = ContentType.objects.get(model=content_type)
-        
-        object_id = int(request.GET.get('object_id'))
         model_class = content_type.model_class()
-        model_object = model_class.objects.get(pk=object_id)
+        model_obj = model_class.objects.get(pk=object_id)
     except ObjectDoesNotExist:
-        return ErrorResponse(401, 'object does not exist')    
+        return ErrorResponse(401, 'object not exist')
 
+    # 处理数据
     if request.GET.get('is_like') == 'true':
-        # 点赞，添加点赞记录，点赞总数+1
+        # 要点赞
         like_record, created = LikeRecord.objects.get_or_create(content_type=content_type, object_id=object_id, user=user)
         if created:
+            # 未点赞过，进行点赞
             like_count, created = LikeCount.objects.get_or_create(content_type=content_type, object_id=object_id)
             like_count.liked_num += 1
             like_count.save()
             return SuccessResponse(like_count.liked_num)
         else:
-            # 已经点赞过了，不能再记录
+            # 已点赞过，不能重复点赞
             return ErrorResponse(402, 'you were liked')
     else:
-        # 取消点赞，删除点赞记录，点赞总数-1
+        # 要取消点赞
         if LikeRecord.objects.filter(content_type=content_type, object_id=object_id, user=user).exists():
+            # 有点赞过，取消点赞
             like_record = LikeRecord.objects.get(content_type=content_type, object_id=object_id, user=user)
             like_record.delete()
+            # 点赞总数减1
             like_count, created = LikeCount.objects.get_or_create(content_type=content_type, object_id=object_id)
             if not created:
                 like_count.liked_num -= 1
                 like_count.save()
                 return SuccessResponse(like_count.liked_num)
             else:
-                return ErrorResponse(403, 'data error')
+                return ErrorResponse(404, 'data error')
         else:
-            # 没有点赞记录，不能操作
-            return ErrorResponse(404, 'you were not liked')
+            # 没有点赞过，不能取消
+            return ErrorResponse(403, 'you were not liked')
