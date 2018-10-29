34bfca84d1721ed57317495450470990c9a04336
haddyyang <624644925@qq.com>
2018-10-29 23:28:16 +0800

    上次第8节课的代码

---
+++ 08.常用的模版标签和过滤器/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# start with blog
+urlpatterns = [
+    # http://localhost:8000/blog/1
+    path('<int:blog_pk>', views.blog_detail, name="blog_detail"),
+    path('type/<int:blog_type_pk>', views.blogs_with_type, name="blogs_with_type"),
+]
