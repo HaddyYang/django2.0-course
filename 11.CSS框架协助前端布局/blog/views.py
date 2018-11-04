5d3ced08e54e50eec97ec35824821d4320231e15
haddyyang <624644925@qq.com>
2018-11-04 23:00:51 +0800

    上传第11节课的代码

---
+++ 11.CSS框架协助前端布局/blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render_to_response, get_object_or_404
+from .models import Blog, BlogType
+
+def blog_list(request):
+    context = {}
+    context['blogs'] = Blog.objects.all()
+    return render_to_response('blog/blog_list.html', context)
+
+def blog_detail(request, blog_pk):
+    context = {}
+    context['blog'] = get_object_or_404(Blog, pk=blog_pk)
+    return render_to_response('blog/blog_detail.html', context)
+
+def blogs_with_type(request, blog_type_pk):
+    context = {}
+    blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
+    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
+    context['blog_type'] = blog_type
+    return render_to_response('blog/blogs_with_type.html', context)
