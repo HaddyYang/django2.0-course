2c9620f4722757aad567d852c046ca9fa91f3a72
HaddyYang <624644925@qq.com>
2019-01-26 01:49:45 +0800

    上传第44节课Part1代码

---
+++ 44.站内消息通知（Part1）/user/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from notifications.signals import notify
+
+
+@receiver(post_save, sender=User)
+def send_notification(sender, instance, **kwargs):
+    if kwargs['created'] == True:
+        verb = '注册成功，更多精彩内容等你发现'
+        notify.send(instance, recipient=instance, verb=verb, action_object=instance)
